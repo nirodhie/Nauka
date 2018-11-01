df02f2c83229ca175332bfdd6d5c5a0c6b6ed29f
nirodhie <lteska2@gmail.com>
2018-11-01 10:28:17 +0100

    added chrome driver

--- webAutomaton.py
+++ webAutomaton.py
@@ -2,7 +2,7 @@
 import time
 from selenium import webdriver
 
-driver = webdriver.Chrome(r"C:\Users\ltesk\Desktop\chromedriver.exe")  # Optional argument, if not specified will search path.
+driver = webdriver.Chrome(r".\browserDrivers\chromedriver.exe")  # Optional argument, if not specified will search path.
 driver.get('http://www.google.com')
 time.sleep(1) # Let the user actually see something!
 search_box = driver.find_element_by_name('q')
