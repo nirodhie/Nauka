ef45a480d8e99b86e2b004182b8d3def2d1843d8
nirodhie <lteska2@gmail.com>
2018-11-01 19:52:01 +0100

    ,

--- webAutomaton.py
+++ webAutomaton.py
@@ -3,10 +3,16 @@ import time
 from selenium import webdriver
 
 driver = webdriver.Chrome(r".\browserDrivers\chromedriver.exe")  # Optional argument, if not specified will search path.
-driver.get('http://www.google.com')
+driver.maximize_window()
+driver.get('https://my.hemmersbach.com')
 time.sleep(1) # Let the user actually see something!
-search_box = driver.find_element_by_name('q')
-search_box.send_keys('python automation is fun')
-search_box.submit()
-time.sleep(5) # Let the user actually see something!
+submitButton = driver.find_element_by_name('SubmitCreds')
+loginTextField = driver.find_element_by_name('username')
+loginTextField.send_keys('LTeska')
+passwordTextField = driver.find_element_by_name('password')
+passwordTextField.send_keys('Hello2017a')
+submitButton.submit()
+myProfile = driver.find_element_by_id('myprofile-win-shortcut')
+myProfile.click()
+#time.sleep(5) # Let the user actually see something!
 #driver.quit()
