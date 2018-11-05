1cb87a0b65d916c945d00d30410a2f90c6258652
nirodhie <lteska2@gmail.com>
2018-11-05 15:06:05 +0100

    .

--- outlookAutomation.py
+++ outlookAutomation.py
@@ -2,14 +2,31 @@ import win32com.client
 
 outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
 
-inbox = outlook.GetDefaultFolder(6) # "6" refers to the index of a folder - in this case,
+#inbox = outlook.GetDefaultFolder(6) # "6" refers to the index of a folder - in this case,
                                     # the inbox. You can change that number to reference
                                     # any other folder
+
+inbox = outlook.Folders['Lukasz.Teska@bd.com'].Folders['Skrzynka odbiorcza'].Folders['New users']
 messages = inbox.Items
 message = messages.GetLast()
-body_content = message.body
-print(body_content)
-print("")
+
+subject = "New user enrollment - "
+while message:
+    if subject in message.Subject:
+        print(message.subject.lstrip(subject)) # usun czesc tytulu ktora poszukujemy bo jest dla wszystkich taka sama
+    message = messages.GetPrevious()
+
+'''
+inbox = outlook.GetDefaultFolder(6)
+messages = inbox.Items
+subject = "Onboarding (Wroclaw) - 19.11.2018"
+message = messages.getfirst()
+while message:
+    message.subject
+    message = messages.GetNext()
+
+'''
+'''
 for i in range(50):
     try:
         box = outlook.GetDefaultFolder(i)
@@ -17,3 +34,4 @@ for i in range(50):
         print(i, name)
     except:
         pass
+'''
