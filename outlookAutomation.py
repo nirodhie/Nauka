bb5c1a0515a4ec08afc82c889870a6fdf241f28a
nirodhie <lteska2@gmail.com>
2018-11-14 14:02:12 +0100

    .

--- outlookAutomation.py
+++ outlookAutomation.py
@@ -1,4 +1,8 @@
 import win32com.client
+from openpyxl import Workbook
+plikExcela = Workbook()
+subject = "Onboarding (Wroclaw) - 01.11.2018"
+skoroszyt = plikExcela.create_sheet(subject)
 
 outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
 
@@ -6,38 +10,32 @@ outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
                                     # the inbox. You can change that number to reference
                                     # any other folder
 
-inbox = outlook.Folders['Lukasz.Teska@bd.com'].Folders['Skrzynka odbiorcza'].Folders['New users']
+inbox = outlook.Folders['Lukasz.Teska@bd.com'].Folders['Skrzynka odbiorcza'] #.Folders['New users']
 messages = inbox.Items
 message = messages.GetLast()
 
-subject = "Onboarding (Wroclaw) - 19.11.2018"
+subject = "Onboarding (Wroclaw) - 01.11.2018"
+skoroszyt = plikExcela.create_sheet(subject)
+cellIteration = 1
+
+
+
 while message:
     if subject in message.Subject and "RE:" not in message.Subject and "FW:" not in message.Subject:
-        print(message.subject.lstrip(subject)) # usun czesc tytulu ktora poszukujemy bo jest dla wszystkich taka sama
-        print(message.body.splitlines()[20])
-        print(message.body.splitlines()[24])
-        print(message.body.splitlines()[28])
-        print(message.body.splitlines()[32])
-        
-        print()
+        realName = message.subject.lstrip(subject)
+           # usun czesc tytulu ktora poszukujemy bo jest dla wszystkich taka sama
+        position = (message.body.splitlines()[20])
+        manager = (message.body.splitlines()[24])
+        globalID = (message.body.splitlines()[28])
+        costCenter = (message.body.splitlines()[32])
+
+        skoroszyt.cell(cellIteration, 1, realName)
+        skoroszyt.cell(cellIteration, 2, position)
+        skoroszyt.cell(cellIteration, 3, manager)
+        skoroszyt.cell(cellIteration, 4, globalID)
+        skoroszyt.cell(cellIteration, 5, costCenter)
+        print(realName, position, manager, globalID, costCenter)
+        cellIteration = cellIteration + 1
     message = messages.GetPrevious()
+plikExcela.save(r'c:\Onboarding_users.xlsx')
 
-'''
-inbox = outlook.GetDefaultFolder(6)
-messages = inbox.Items
-subject = "Onboarding (Wroclaw) - 19.11.2018"
-message = messages.getfirst()
-while message:
-    message.subject
-    message = messages.GetNext()
-
-'''
-'''
-for i in range(50):
-    try:
-        box = outlook.GetDefaultFolder(i)
-        name = box.Name
-        print(i, name)
-    except:
-        pass
-'''
